f5f268e8f965c5dd953233194d7f0c7a6c34d4c4
Linus Björklund <weedzcokie@gmail.com>
2018-07-02 21:11:52 +0200

    more modules

--- index.js
+++ index.js
@@ -1,47 +1,11 @@
 "use strict";
-const readline = require('readline');
 
 const config = require('./config.json');
-const commands = require('./ts3/commands.json');
 
-// commandline interface
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: '> ',
-    completer: function(line) {
-        const completions = Object.keys(commands);
-        const hits = completions.filter((c) => c.startsWith(line));
-        // show all completions if none found
-        return [hits.length ? hits : completions, line];
-    }
-}).on('line', line => {
-    const [cmd, ...args] = line.trim().split(" ");
-    if (0 && !client.connected) {
-        // Nothing..
-    } else {
-        if (cmd === 'help') {
-            if (args.length === 1) {
-                client.showHelp(args[0]);
-            } else {
-                client.showHelp();
-            }
-        } else if (cmd === 'login') {
-            if (args.length === 2) {
-                client.login(args[0], args[1]);
-            } else if (args.length === 0) {
-                client.login(config.auth.username, config.auth.password);
-            } else {
-                console.warn("Needs 2 parameters: login <username> <password>");
-            }
-        } else if (cmd === 'useserver') {
-            client.useServer(1);
-        } else if (cmd === 'autosetup') {
-            client.autoSetup();
-        } else {
-            client.sendCmd(cmd, args);
-        }
-    }
-    rl.prompt();
-});
-const client = require('./ts3/Client')(rl, config);
+const Connection = require('./lib/Connection')(config);
+
+const Client = require('./lib/Client')(Connection, config);
+
+const CLI = require('./lib/CLI')(Client, config);
+
+Connection.init(CLI, Client);
