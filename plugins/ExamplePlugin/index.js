7e7cd71860de9bd7b717cc2f09b0bb6f5acc793a
Linus Björklund <weedzcokie@gmail.com>
2018-07-04 17:47:24 +0200

    change send() to return a Promise

--- plugins/ExamplePlugin/index.js
+++ plugins/ExamplePlugin/index.js
@@ -1,10 +1,13 @@
 module.exports = function(connection) {
     console.log("Hello world!");
     if (connection.connected()) {
-        connection.send('use',[1], {mustReturnOK:true}, function(params) {
-            connection.send('whoami', undefined, {mustReturnOK:true}, function(params) {
-                console.log(params);
-            })
+        Promise.all([
+            connection.send('use', [1], {mustReturnOK: true, noOutput: true}),
+            connection.send('whoami', undefined, {mustReturnOK: true, noOutput: true})
+        ]).then(data => {
+            console.log("ExamplePlugin: ", data);
+        }).catch(data => {
+            console.log("ExamplePlugin catch: ", data);
         });
     } else {
         console.log('Not connected...');
