f7c44df3f1b05a3ad760f3fe82ede599f6b0723d
Linus Björklund <weedzcokie@gmail.com>
2018-07-04 22:27:28 +0200

    Add priority to command queue We can queue up alot of tasks with priority 2 but a command with priority 0 will always execute as soon as possible

--- plugins/ExamplePlugin/index.js
+++ plugins/ExamplePlugin/index.js
@@ -5,16 +5,25 @@ class ExamplePlugin {
     connected(connection) {
         this.connection = connection;
         Promise.all([
-            connection.send('use', [1], {mustReturnOK: true, noOutput: true}),
-            connection.send('whoami', undefined, {mustReturnOK: true, noOutput: true})
+            this.connection.send('use', [1], {mustReturnOK: true, noOutput: true}),
+            this.connection.send('whoami', undefined, {mustReturnOK: true, noOutput: true})
         ]).then(data => {
-            console.log("ExamplePlugin: ", data);
+            console.log("ExamplePlugin - ", data);
         }).catch(data => {
-            console.log("ExamplePlugin catch: ", data);
+            console.log("ExamplePlugin - Catch: ", data);
         });
     }
     setup() {
         console.log("We in there bois!");
+        // get clientlist every 5 second
+        const connection = this.connection;
+        function fetchClientList() {
+            connection.send('clientlist', undefined, {noOutput: true}, 2).then(data => {
+                console.log(`ExamplePlugin - clients: ${data.length}`);
+                setTimeout(fetchClientList, 5000);
+            });
+        }
+        setTimeout(fetchClientList, 5000);
     }
 }
 
