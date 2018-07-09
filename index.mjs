a709d455bf6c7d2c46ed7aff3201d457ac46372b
Linus Björklund <weedzcokie@gmail.com>
2018-07-10 00:08:34 +0200

    Some refactoring Move actual loading (import) of plugin to a new function

--- index.mjs
+++ index.mjs
@@ -1,13 +1,10 @@
-"use strict";
-
 import config from './config.json';
 import Client from './lib/Client';
 import Connection from './lib/Connection';
 import CLI from './lib/CLI';
 
 const connection = new Connection(config);
+connection.init();
 const client = new Client(connection, config);
 
-const cli = CLI(client, connection, config);
-
-connection.init(cli);
+CLI(client, connection, config);
