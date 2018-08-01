3833b5400403bae8791e8b3df774522f36386fbd
Linus Björklund <weedzcokie@gmail.com>
2018-08-01 18:44:11 +0200

    Change CLI to a plugin
    CLI can now also be turned off.
    Some refactoring on plugin handling.

--- index.mjs
+++ index.mjs
@@ -1,9 +1,18 @@
 import config from './config.json';
 import Client from './lib/Client';
 import Connection from './lib/Connection';
-import CLI from './lib/CLI';
 
 const connection = new Connection(config);
 const client = new Client(connection, config);
 
-CLI(client, connection, config);
+// parse commandline arguments
+const args = {};
+for (let argv of process.argv.splice(2)) {
+    const [key, value] = argv.split("=");
+    args[key] = value !== undefined ? value : true;
+}
+if (args['--init']) {
+    connection.connection.on('connect', () => {
+        client.init();
+    });
+}
