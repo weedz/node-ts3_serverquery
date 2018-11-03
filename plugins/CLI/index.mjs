bc4fdc69a01b47cb7a12b05111afceb30385eb0e
Linus Björklund <weedzcokie@gmail.com>
2018-11-03 15:47:07 +0100

    Refactor

--- plugins/CLI/index.mjs
+++ plugins/CLI/index.mjs
@@ -6,7 +6,7 @@ export const VERSION = 1;
 
 export default class CLI extends Plugin {
     constructor() {
-        super();
+        super({});
         this.commands = {};
         this.registerCommand('init', () => {
             this.client.init();
@@ -39,67 +39,87 @@ export default class CLI extends Plugin {
             this.client.unloadPlugin(plugin);
         });
 
+        this.handleAutoCompletion = this.handleAutoCompletion.bind(this);
+        this.onLine = this.onLine.bind(this);
+
         // commandline interface
-        const rl = createInterface({
+        this.rl = createInterface({
             input: process.stdin,
             output: process.stdout,
             prompt: '> ',
-            completer: line => {
-                line = line.split(" ");
-                const completions = this.connection.connected()
-                    ? Object.keys(commands).concat(Object.keys(this.commands))
-                    : [];
-                const hits = completions.filter(c => c.startsWith(line[0]));
-
-                if (commands[line[0]]) {
-                    process.stdout.write(`\nDescription: ${commands[line[0]].description}\n`);
-                    if (commands[line[0]].params) {
-                        process.stdout.write(`Params:\n`);
-                        for (let param of commands[line[0]].params) {
-                            process.stdout.write(`\t${param.key} = ${param.value}`);
-                        }
-                        process.stdout.write("\n");
-                    }
-                }
-
-                // show all completions if none found
-                return [hits.length ? hits : completions, line[0]];
-            }
-        });
-        rl.on('line', async line => {
-            const [cmd, ...args] = line.trim().split(" ");
-            if (!this.connection.connected()) {
-                const status = this.connection.connecting() ? "Connecting" : "Disconnected";
-                console.log(`Status: ${status}`);
-            } else {
-                if (this.isCommand(cmd)) {
-                    await this.executeCommand(cmd, args);
-                } else if (cmd === 'help') {
-                    if (args.length === 1) {
-                        await this.client.showHelp(args[0]);
-                    } else {
-                        await this.client.showHelp();
-                    }
-                } else if (cmd === 'login') {
-                    if (args.length === 2) {
-                        await this.client.login(args[0], args[1]);
-                    } else {
-                        console.warn("Needs 2 parameters: login <username> <password>");
-                    }
-                } else {
-                    try {
-                        const param = await this.connection.send(cmd, args);
-                        console.log("Result:", param);
-                    } catch(err) {
-                        console.warn("Error: ", err);
-                    }
-                }
-            }
-            rl.prompt();
-        });
+            completer: this.handleAutoCompletion
+        })
+        .on('line', this.onLine);
     }
-    init() {
-        console.log("We in there bois!");
+
+    handleAutoCompletion(line) {
+        line = line.split(" ");
+        const completions = this.connection.connected()
+            ? Object.keys(commands).concat(Object.keys(this.commands))
+            : [];
+        const hits = completions.filter(c => c.startsWith(line[0]));
+
+        if (commands[line[0]]) {
+            this.autoCompleteCommand(commands[line[0]]);
+        }
+
+        // show all completions if none found
+        return [hits.length ? hits : completions, line[0]];
+    }
+
+    autoCompleteCommand(cmd) {
+        process.stdout.write(`\nDescription: ${cmd.description}\n`);
+        if (cmd.params) {
+            process.stdout.write(`Params:\n`);
+            for (let param of cmd.params) {
+                process.stdout.write(`\t${param.key} = ${param.value}`);
+            }
+            process.stdout.write("\n");
+        }
+    }
+
+    async onLine(line) {
+        const [cmd, ...args] = line.trim().split(" ");
+        if (!this.connection.connected()) {
+            const status = this.connection.connecting() ? "Connecting" : "Disconnected";
+            console.log(`Status: ${status}`);
+        } else {
+            if (this.isCommand(cmd)) {
+                await this.executeCommand(cmd, args);
+            } else if (cmd === 'help') {
+                this.handleHelpCommand(args)
+            } else if (cmd === 'login') {
+                this.handleLoginCommand(args);
+            } else {
+                this.sendCommand(cmd, args);
+            }
+        }
+        this.rl.prompt();
+    }
+
+    async handleHelpCommand(args) {
+        if (args.length === 1) {
+            await this.client.showHelp(args[0]);
+        } else {
+            await this.client.showHelp();
+        }
+    }
+
+    async handleLoginCommand(args) {
+        if (args.length === 2) {
+            await this.client.login(args[0], args[1]);
+        } else {
+            console.warn("Needs 2 parameters: login <username> <password>");
+        }
+    }
+
+    async sendCommand(cmd, args) {
+        try {
+            const param = await this.connection.send(cmd, args);
+            console.log("Result:", param);
+        } catch(err) {
+            console.warn("Error: ", err);
+        }
     }
 
     registerCommand(cmd, callback) {
@@ -115,5 +135,6 @@ export default class CLI extends Plugin {
         if (this.isCommand(cmd)) {
             return this.commands[cmd](...args);
         }
+        return false;
     }
 }
