7026380f9877718a9aa234a50c90cd201089d895
Linus Björklund <weedzcokie@gmail.com>
2019-02-17 04:18:52 +0100

    Fix cache
    Cache clientlist/clientinfo/channellist/channelinfo for 1 sec. Subsequent requests will get data from cache. 
    Cached data must not be older than 1 second.

--- lib/Connection/DataStore.ts
+++ lib/Connection/DataStore.ts
@@ -1,5 +1,6 @@
 import { TSCommandList } from '../commands';
 import Connection from '../Connection';
+import Log from '../Log';
 
 type ListCache = 
     |"clientlist"
@@ -14,6 +15,8 @@ const ItemKeyMap: {[listName in ItemCache]: keyof TSCommandList[listName]} = <co
     channelinfo: "cid"
 };
 
+const Cache = new Map();
+
 export default class DataStore {
     connection: Connection;
 
@@ -21,14 +24,42 @@ export default class DataStore {
         this.connection = connection;
     }
 
-    fetchList(cacheName: ListCache) {
-        return this.connection.send<typeof cacheName>(cacheName);
+    async fetchList(cacheName: ListCache) {
+        let currentCache = Cache.get(cacheName);
+        const time = Date.now();
+        if (!currentCache || currentCache.time < time + 1000) {
+            currentCache = {
+                time,
+                data: await this.connection.send<typeof cacheName>(cacheName)
+            };
+            Cache.set(cacheName, currentCache);
+        } else {
+            Log(`Cached data: ${cacheName}`, "DataStore", 5);
+        }
+        return Promise.resolve(currentCache.data);
     }
 
-    fetchItem(cacheName: ItemCache, id:number) {
+    async fetchItem(cacheName: ItemCache, id:number) {
         const key = ItemKeyMap[cacheName];
-        // typeguarded by ItemKeyMap
-        return this.connection.send<typeof cacheName>(cacheName, <any>{ [key]: id });
+        let cacheList = Cache.get(cacheName);
+        let cache;
+        const time = Date.now();
+        if (cacheList) {
+            cache = cacheList.get(id);
+        } else {
+            cacheList = new Map();
+        }
+        if (!cache || cache.time < time + 1000) {
+            cache = {
+                time,
+                // typeguarded by ItemKeyMap
+                data: await this.connection.send<typeof cacheName>(cacheName, <any>{ [key]: id })
+            };
+            cacheList.set(id, cache);
+        } else {
+            Log(`Cached data: ${cacheName}, id=${id}`, "DataStore", 5);
+        }
+        return Promise.resolve(cache.data);
     }
 
     forceUpdateItem() {
